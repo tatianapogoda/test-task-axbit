35911507f6531a5fa1ed5127cafab3e3e3b2a3ff
tatianapogoda <pogoda277@gmail.com>
2020-07-27 09:32:42 +0400

    SQL query

--- src/main/resources/database/initDB.sql
+++ src/main/resources/database/initDB.sql
@@ -15,5 +15,27 @@ CREATE TABLE IF NOT EXISTS genre (
 CREATE TABLE IF NOT EXISTS book (
 	id BIGSERIAL PRIMARY KEY,
 	ISBN CHAR(17) NOT NULL,
-	genre BIGINT REFERENCES genre(id) NOT NULL
+	genre_id BIGINT NOT NULL,
+	author_id BIGINT NOT NULL,
+	CONSTRAINT GENGE_FOREIGN_KEY FOREIGN KEY (genre_id) REFERENCES genre(id),
+	CONSTRAINT AUTHOR_FOREIGN_KEY FOREIGN KEY (author_id) REFERENCES author(id)
 );
+
+CREATE RULE delete_author AS
+    ON DELETE TO public.author
+    WHERE (books <> ARRAY[NULL])
+    DO INSTEAD NOTHING;
+
+insert into author values
+(1, 'Александр','Сергеевич','Пушкин','1799-06-05','{1,2}'),
+(1, 'Сергей','Васильевич','Лукьяненко','1968-04-11','{3,4}');
+
+insert into genre values
+(1,'фантастика'),
+(2,'поэзия');
+
+insert into book values
+(1,'978-5-17-098131-1',1,10),
+(2,'978-5-17-112584-4',1,10),
+(3,'978-5-353-07209-6',2,1),
+(4,'978-5-389-04903-1',2,1);
